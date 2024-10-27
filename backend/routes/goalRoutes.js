5d5536acac81dffe7da8503ffdd7fe06b990e190
xJuhoo <juho.rekonen@student.lut.fi>
2024-10-27 11:38:37 +0200

    Added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,9 +6,10 @@ const {
     updateGoal, 
     deleteGoal 
 } = require("../controllers/goalController")
+const { protect } = require('../middleware/authMiddleware');
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 // router.get("/", getGoals);
 
